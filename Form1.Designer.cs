e01e30004e765e4d9bc8fea415949dfd4cc2defc
Fro-Z <fro-z@email.cz>
2019-03-27 13:53:39 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace DiceForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.diceBox = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dieSides_3 = new System.Windows.Forms.NumericUpDown();
+            this.dieSides_2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dieSides_0 = new System.Windows.Forms.NumericUpDown();
+            this.dieSides_1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.discardUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultsList = new System.Windows.Forms.ListView();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.diceBox.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discardUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.diceBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.progressBar);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.resultsList);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 523);
+            this.splitContainer1.SplitterDistance = 133;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // diceBox
+            // 
+            this.diceBox.Controls.Add(this.tableLayoutPanel1);
+            this.diceBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.diceBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.diceBox.Location = new System.Drawing.Point(0, 0);
+            this.diceBox.Name = "diceBox";
+            this.diceBox.Size = new System.Drawing.Size(133, 447);
+            this.diceBox.TabIndex = 1;
+            this.diceBox.TabStop = false;
+            this.diceBox.Text = "Dice";
+            this.diceBox.Enter += new System.EventHandler(this.diceBox_Enter);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.dieSides_3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.dieSides_2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dieSides_0, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dieSides_1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.discardUpDown, 0, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(127, 419);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // dieSides_3
+            // 
+            this.dieSides_3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dieSides_3.Location = new System.Drawing.Point(3, 143);
+            this.dieSides_3.Name = "dieSides_3";
+            this.dieSides_3.Size = new System.Drawing.Size(121, 29);
+            this.dieSides_3.TabIndex = 7;
+            this.dieSides_3.ValueChanged += new System.EventHandler(this.dieSides_3_ValueChanged);
+            // 
+            // dieSides_2
+            // 
+            this.dieSides_2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dieSides_2.Location = new System.Drawing.Point(3, 103);
+            this.dieSides_2.Name = "dieSides_2";
+            this.dieSides_2.Size = new System.Drawing.Size(121, 29);
+            this.dieSides_2.TabIndex = 6;
+            this.dieSides_2.ValueChanged += new System.EventHandler(this.dieSides_2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(42, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sides";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dieSides_0
+            // 
+            this.dieSides_0.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dieSides_0.Location = new System.Drawing.Point(3, 23);
+            this.dieSides_0.Name = "dieSides_0";
+            this.dieSides_0.Size = new System.Drawing.Size(121, 29);
+            this.dieSides_0.TabIndex = 3;
+            this.dieSides_0.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.dieSides_0.ValueChanged += new System.EventHandler(this.dieSides_0_ValueChanged);
+            // 
+            // dieSides_1
+            // 
+            this.dieSides_1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dieSides_1.Location = new System.Drawing.Point(3, 63);
+            this.dieSides_1.Name = "dieSides_1";
+            this.dieSides_1.Size = new System.Drawing.Size(121, 29);
+            this.dieSides_1.TabIndex = 5;
+            this.dieSides_1.ValueChanged += new System.EventHandler(this.dieSides_1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(3, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 40);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Discard";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // discardUpDown
+            // 
+            this.discardUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.discardUpDown.Location = new System.Drawing.Point(3, 223);
+            this.discardUpDown.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.discardUpDown.Name = "discardUpDown";
+            this.discardUpDown.Size = new System.Drawing.Size(121, 29);
+            this.discardUpDown.TabIndex = 9;
+            this.discardUpDown.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(271, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Results";
+            // 
+            // resultsList
+            // 
+            this.resultsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.resultsList.Location = new System.Drawing.Point(16, 28);
+            this.resultsList.Name = "resultsList";
+            this.resultsList.Size = new System.Drawing.Size(635, 453);
+            this.resultsList.TabIndex = 0;
+            this.resultsList.UseCompatibleStateImageBehavior = false;
+            this.resultsList.View = System.Windows.Forms.View.Details;
+            this.resultsList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.resultsList_KeyDown);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(16, 487);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(635, 22);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Dice roll probabilities";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.diceBox.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dieSides_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discardUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox diceBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown dieSides_0;
+        private System.Windows.Forms.NumericUpDown dieSides_1;
+        private System.Windows.Forms.NumericUpDown dieSides_3;
+        private System.Windows.Forms.NumericUpDown dieSides_2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView resultsList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown discardUpDown;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Random rnd = new Random();
+
+        private const int THROWS_PER_UPDATE = THROWS_LIMIT/(2*10);
+        private const int THROWS_LIMIT = 20000000;
+
+        private List<int> dice = new List<int>();
+        private int discardCount = 0;
+
+        // How many times each value was rolled
+        private List<Int32> counts = new List<int>();
+        private Int64 numThrows = 0;
+
+        private CultureInfo cultureInfo = new CultureInfo("en-GB");
+
+        private int totalMaxDieValue
+        {
+            get { return dice.Sum(); }
+        }
+
+        private int diesToCount
+        {
+            get { return dice.Count - discardCount; }
+        }
+
+        public void updateListView()
+        {
+            resultsList.Clear();
+            resultsList.FullRowSelect = true;
+
+            resultsList.Columns.Add("", 80);
+            for (int i = 1; i <= totalMaxDieValue; i++)
+            {
+                resultsList.Columns.Add(i.ToString(), 50);
+            }
+
+            ListViewItem item = new ListViewItem("Probabilities");
+            for (int i = 1; i < counts.Count; i++)
+            {
+                double count = counts[i];
+                item.SubItems.Add((count*100 / Convert.ToDouble(numThrows)).ToString("0.00", cultureInfo));
+            }
+
+            resultsList.Items.Add(item);
+
+        }
+
+        public void updateData()
+        {
+            if (numThrows > THROWS_LIMIT)
+                return;
+            
+            for (int i = 0; i < THROWS_PER_UPDATE; i++)
+            {
+                List<int> throwResults = new List<int>();
+                foreach (int sides in dice)
+                {
+                    throwResults.Add(rnd.Next(1, sides + 1));               
+                }
+
+                throwResults.Sort();
+
+                int sum = 0;
+                // Take the best results and store them
+                for (int j = discardCount; j < throwResults.Count; j++)
+                {
+                    sum += throwResults[j];
+                }
+
+                counts[sum]++;
+                numThrows++;
+            }
+
+            double progressRatio = Convert.ToDouble(numThrows) / THROWS_LIMIT;
+            progressBar.Value = Math.Min(Convert.ToInt32(Math.Ceiling(progressRatio*progressBar.Maximum)), progressBar.Maximum);
+        }
+
+        private void ResetStats()
+        {
+            counts.Clear();
+            numThrows = 0;
+            for (int i = 0; i <= totalMaxDieValue; i++)
+            {
+                counts.Add(0);
+            }
+
+            progressBar.Value = 0;
+        }
+
+        public void OnDiceChange()
+        {
+            dice.Clear();
+            dice.Add(Convert.ToInt32(dieSides_0.Value));
+            dice.Add(Convert.ToInt32(dieSides_1.Value));
+            dice.Add(Convert.ToInt32(dieSides_2.Value));
+            dice.Add(Convert.ToInt32(dieSides_3.Value));
+
+            dice.RemoveAll(e => e == 0);
+
+            // Update discard counter
+            int maxDiscardCount = Math.Max(dice.Count - 1, 0);
+            discardUpDown.Minimum = 0;
+            discardUpDown.Maximum = maxDiscardCount;
+            discardUpDown.Value = Math.Min(Math.Max(discardUpDown.Minimum, discardUpDown.Value), discardUpDown.Maximum);
+
+            ResetStats();
+        }
+
+        public void onDiscardChange()
+        {
+            discardCount = Convert.ToInt32(discardUpDown.Value);
+            ResetStats();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void cb_adv2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void diceBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dieSides_0_ValueChanged(object sender, EventArgs e)
+        {
+            OnDiceChange();
+        }
+
+        private void dieSides_1_ValueChanged(object sender, EventArgs e)
+        {
+            OnDiceChange();
+        }
+
+        private void dieSides_2_ValueChanged(object sender, EventArgs e)
+        {
+            OnDiceChange();
+        }
+
+        private void dieSides_3_ValueChanged(object sender, EventArgs e)
+        {
+            OnDiceChange();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Timer timer = new Timer();
+            timer.Interval = (100); // 100ms
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+            OnDiceChange();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            if (counts.Count > 0 && dice.Count > 0 && numThrows<THROWS_LIMIT)
+            {
+                updateData();      
+                updateListView();
+            }
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            onDiscardChange();
+        }
+
+        private void resultsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender != resultsList) return;
+
+            if (e.Control && e.KeyCode == Keys.C)
+                CopySelectedValuesToClipboard();
+        }
+
+        private void CopySelectedValuesToClipboard()
+        {
+            var builder = new StringBuilder();
+            foreach (ListViewItem item in resultsList.SelectedItems)
+            {
+                foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                {
+                    builder.AppendFormat("{0} \t", subitem.Text);
+                }
+                
+            }
+                
+
+            Clipboard.SetText(builder.ToString());
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
